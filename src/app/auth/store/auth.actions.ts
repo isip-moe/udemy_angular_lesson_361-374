4d49b1d4da20ad57932ecaf54964238465f56b6e
dannyLiu <h152074716@gmail.com>
2023-03-04 16:06:13 +0800

    377:Effects & Error Handing
    新增 auth.effects.ts
    修改 auth.actions.ts
    - 加入 const LOGIN_START = "[Auth] Login Start";

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,34 +1,34 @@
-import { Action } from "@ngrx/store"
+import { Action } from "@ngrx/store";
 
-export const LOGIN = "[Auth] Login"
-export const LOGOUT = "[Auth] Logout"
-
+export const LOGIN_START = "[Auth] Login Start";
+export const LOGIN = "[Auth] Login";
+export const LOGOUT = "[Auth] Logout";
 
 export class Login implements Action {
   readonly type = LOGIN;
   payload: {
-    email: string,
-    userId: string,
-    token: string,
-    expirationDate: Date
-  }
+    email: string;
+    userId: string;
+    token: string;
+    expirationDate: Date;
+  };
   constructor(payload: {
-    email: string,
-    userId: string,
-    token: string,
-    expirationDate: Date
+    email: string;
+    userId: string;
+    token: string;
+    expirationDate: Date;
   }) {
-    this.payload = payload
+    this.payload = payload;
   }
-
 }
 
+export class LoginStart implements Action {
+  readonly type = LOGIN_START;
+  constructor(public payload: { email: string; password: string }) {}
+}
 
 export class Logout implements Action {
   readonly type = LOGOUT;
 }
 
-
-export type AuthActions =
-  Login |
-  Logout;
+export type AuthActions = Login | Logout;
---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from "@angular/common/http";
+import * as AuthActions from "./auth.actions";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { AuthResponseData } from "../auth.service";
+import { catchError, map, switchMap } from "rxjs/operators";
+
+export class AuthEffects {
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http
+        .post<AuthResponseData>(
+          "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCBo0hOkuRCM0kTXJCUqcTtKDLpFLldt30",
+          {
+            email: authData.payload.email,
+            password: authData.payload.password,
+            returnSecureToken: true,
+          }
+        )
+        .pipe(
+          catchError((error) => {
+            of();
+          }),
+          map((resData) => {
+            of();
+          })
+        );
+    })
+  );
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}
